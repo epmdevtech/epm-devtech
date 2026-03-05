45f0f67f7ba39a2df289a95dee104a33b5f3211b
elessandro <elessandrodev@gmail.com>
2026-03-05 08:32:24 -0300

    build: otimiza chunk splitting e configuração do vite

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 }));
